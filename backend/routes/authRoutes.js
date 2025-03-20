a07cf81828660befc0dd3183c9182fff76b5391f
Ragul Karthick G M <120189432+ragulkarthick832@users.noreply.github.com>
2025-03-20 10:45:31 +0530

    Delete backend directory

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { register, login, verifyEmail } = require("../controllers/authController");
-const router = express.Router();
-
-router.post("/auth/register", register);
-router.post("/auth/login", login);
-router.get("/auth/verify-email/:token", verifyEmail);
-
-module.exports = router;
